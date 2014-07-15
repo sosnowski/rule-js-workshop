c759d41197f2678f4572245af7ed898415757a5b
Damian Sosnowski <sosnowski@outlook.com>
2014-07-15 22:16:28 +0200

    Lesson3 + new operator exercise

---
+++ Lesson3/code/new_operator/newOperator.js
@@ -0,0 +1,10 @@
+(function (global) {
+	if (!global.rule) {
+		global.rule = {};
+	}
+	
+	//Implement function that will work like a new operator;
+	rule.createClassInstance = function (ClassFunction) {
+		
+	};
+}(window));
