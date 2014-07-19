42a3bd232e85d3eb424e9f00dcf4074b11315fdc
Damian Sosnowski <sosnowski@outlook.com>
2014-07-19 19:12:20 +0200

    newOperator implementation with the description, template for event emitter.

--- Lesson3/code/event_emitter/ee.js
+++ Lesson3/code/event_emitter/ee.js
@@ -6,8 +6,34 @@
 	}
 	
 	EE = function () {
+		//store the listeners somewhere
+		this.listeners = {};
+	};
+	
+	EE.prototype.on = function (eventName, listener, context) {
+		
+		return function () {
+			
+		}
+	};
+	
+	EE.prototype.emit = function (eventName, data) {
+		
 		
 	};
+	
+//	var ee = new EE();
+//	
+//	var removeListener = ee.on('test', function (arg1, arg2) {
+//		console.log(arg1, arg2); //1, 2
+//		console.log(this.key); //value
+//	}, { key: 'value' });
+//	
+//	ee.emit('test', [1, 2]);
+//	
+//	removeListener(); //removes my listener from the event emitter;
+//	
+//	ee.emit('test'); //nothing will execute
 	
 	global.rule.EventEmitter = EE;
 	
--- Lesson3/code/new_operator/newOperator.js
+++ Lesson3/code/new_operator/newOperator.js
@@ -5,6 +5,21 @@
 	
 	//Implement function that will work like a new operator;
 	rule.createClassInstance = function (ClassFunction) {
+		//Create an object with a given prototype (ClassFunction.prototype)
+		//We are creating an object. The prototype of the newly created object is set to "ClassFunction.prototype"
+		var obj = Object.create(ClassFunction.prototype), result;
 		
+		//We need to execute constructor function in a proper context. So the "this" value is pointing to our object.
+		//We can use apply method. apply method allows us to call function with a custom context.
+		//Additionally we are passing an array of arguments to the function
+		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+		result = ClassFunction.apply(obj, Array.prototype.slice.call(arguments, 1));
+		
+		//If constructor returns some object, return it. If not, return the initially created object
+		if(result !== undefined) {
+			return result;
+		} else {
+			return obj;
+		}
 	};
 }(window));
